505e31af504820bd86154ad0d5b70f7a4397a9b0
Neslihan Serengil <neslihangunal@gmail.com>
2022-02-27 14:40:25 +0300

    Init

---
+++ IAmRich/IAmRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Neslihan Günal on 27.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
